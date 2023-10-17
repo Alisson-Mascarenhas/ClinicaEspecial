fa3af54f7f86164fbbaea44442d78ac86a5a048b
Alisson <alisson.oliveira@ucsal.edu.br>
2023-10-17 09:29:18 -0300

    Adicionando primeiro comite

---
+++ src/main/java/br/ucsal/clinicaespecial/ClinicaEspecialApplication.java
@@ -0,0 +1,13 @@
+package br.ucsal.clinicaespecial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaEspecialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaEspecialApplication.class, args);
+	}
+
+}
